c7e0ea45f5e598abb9d19b09b7212217440a5f4c
ashe.im <ashe.im@kakaocorp.com>
2020-01-21 17:46:08 +0900

    Wrapping a REST API with Graph ql

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { GraphQLServer } from "graphql-yoga";
-import resolvers from "./graphql/resolver";
+import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './graphql/resolver';
 
 const server = new GraphQLServer({
-  typeDefs: "graphql/schema.graphql",
+  typeDefs: 'graphql/schema.graphql',
   resolvers
 });
 
-server.start(() => console.log("Server is running on localhost:4000"));
+server.start(() => console.log('Server is running on localhost:4000'));
