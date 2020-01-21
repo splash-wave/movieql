3567da8ef681ea269b7b83b7bec7d6b1b558def7
ashe.im <ashe.im@kakaocorp.com>
2020-01-21 14:12:32 +0900

    Creating First Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,51 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: "Ashe",
-    age: 32,
-    gender: "female"
+    name: "Star Wars - The new one",
+    score: 16
   },
   {
     id: 1,
-    name: "John",
-    age: 45,
-    gender: "male"
+    name: "Avengers - The new one",
+    score: 23
   },
   {
     id: 2,
-    name: "Todd",
-    age: 24,
-    gender: "male"
+    name: "The GodFather 1",
+    score: 88
   },
   {
-    id: 4,
-    name: "Sam",
-    age: 60,
-    gender: "female"
+    id: 3,
+    name: "Logan",
+    score: 45
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+//deleteMovie 같은 아이디를 가지지 않은 movies의 배열 리턴
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//addMovie
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
